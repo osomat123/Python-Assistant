1f137e9f5a52015c6d5ebfab30469ec1f23daf76
osomat123 <tejpunjraju8a@gmail.com>
2020-08-19 03:49:10 +0530

    Version 1.2: Added loop, added help text, optimized application search, added feature for app nicknames

--- OpenApps.py
+++ OpenApps.py
@@ -1,46 +1,120 @@
 import os
 import pyttsx3
+import json
 
 def getApps():
-    apps = {}
     
-    for (path,app,file) in os.walk('/Applications'):
-        if path.endswith('.app') and path.count('.app') == 1:
-            app = path[1:].split('/')[-1].replace('.app','')  # Get app name
-            path = path.replace(' ','\ ') # Set app path
-            apps[app] = path
+    try:
+        with open("apps.json","r") as read_file:
+            strApps = read_file.read()
+            apps = eval(json.loads(strApps))
+     
+    except FileNotFoundError:
         
-    for (path,app,file) in os.walk('/System/Applications'):
-        if path.endswith('.app') and path.count('.app') == 1:
-            app = path[1:].split('/')[-1].replace('.app','') # Get app name
-            path = path.replace(' ','\ ') # Set app path
-            apps[app] = path
+        apps = {}
+
+        for (path,app,file) in os.walk('/Applications'):
+            if path.endswith('.app') and path.count('.app') == 1:
+                app = path[1:].split('/')[-1].replace('.app','')  # Get app name
+                path = path.replace(' ','\ ') # Set app path
+                apps[app] = path
+
+        for (path,app,file) in os.walk('/System/Applications'):
+            if path.endswith('.app') and path.count('.app') == 1:
+                app = path[1:].split('/')[-1].replace('.app','') # Get app name
+                path = path.replace(' ','\ ') # Set app path
+                apps[app] = path
+                
+        with open("apps.json",'w') as file:
+            file.write(json.dumps(str(apps)))
+    
+    return apps
+
+
+def addNick(apps,nick,longApp):
+    
+    flag = 0
+    
+    for app in apps.keys():
+        if app.upper() == longApp:
+            apps[longApp] = apps[app]
+            flag = 1
+    
+    if flag ==0:
+        return None
+    
+    with open("apps.json",'w') as file:
+        file.write(json.dumps(str(apps)))
     
     return apps
 
 
 print("Initializing...\n")
+valid = ['RUN','OPEN','EXECUTE','BYE','EXIT','QUIT','ADD NICKNAME','HELP','STOP']
 apps = getApps()
 
-print("Welcome to tools")
-pyttsx3.speak("Welcome to tools")
-
+print("Welcome to Py Assistant")
 print("How can I help?\n")
-pyttsx3.speak("How can I help?")
+pyttsx3.speak("Welcome to Py Assistant....How can I help?")
 
-query = input("Type here -> ").upper()
-command = ''
+print("Type 'help' if you're confused\n")
+pyttsx3.speak("Type help if you're confused")
 
-for app in apps.keys():
-    if app.upper() in query:
-        command = 'open '+apps[app]
+while True:
+    
+    query = input("Type here -> ").upper()
+    appFound = 0
+    commandValid = 0
+    
+    for command in valid:
+        if command in query:
+            commandValid = 1
+            break
+    
+    if commandValid == 0:
+        print('Sorry, operation not supported\n')
+        pyttsx3.speak('Sorry, operation not supported.')
+        continue
+
+    if "HELP" in query:
+        f = open("help.txt",'r')
+        print(f.read())
+        f.close()
+        continue
         
-        print("Opening "+app+" ...")
-        pyttsx3.speak("Opening "+app)
-        
-        os.system(command)
+    if ("BYE" == query) or ("EXIT" == query) or ("QUIT" == query) or ('STOP' == query):
+        print("Bye!\n")
+        pyttsx3.speak("Bye.")
         break
         
-if command == '':
-    print('Application not found. Try Again!')
-    pyttsx3.speak("Application not found. Try Again!")
+    if 'ADD NICKNAME'in query:
+        names = [i.strip() for i in query.split('FOR')]
+        nick = names[0][13:]
+        longApp = names[1]
+        
+        if nick in apps:
+            print("Nickname already exists. Give another nickname.\n")
+            pyttsx3.speak("Nickname already exists. Give another nickname")
+            continue
+        
+        ret = addNick(apps,nick,longApp)
+        
+        if ret != None:
+            app = ret
+            print("Nickname added.\n")
+            pyttsx3.speak("Nickname added.")
+            break
+            
+    if ('RUN' in query) or ('OPEN' in query) or ('EXECUTE' in query):
+        for app in apps.keys():
+            if app.upper() in query:
+                command = 'open '+apps[app]
+                print("Opening "+app+" ...\n")
+                pyttsx3.speak("Opening "+app)
+                os.system(command)
+                appFound = 1
+                break        
+
+    if appFound == 0:
+        print('Application not found. Try Again!\n')
+        pyttsx3.speak("Application not found. Try Again!")
